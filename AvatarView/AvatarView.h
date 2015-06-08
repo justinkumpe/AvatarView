aa47943ee382d74bc4f86010ff1de28363ae6637
chrene <chrene91@gmail.com>
2015-06-08 22:12:15 +0200

    Initial Commit

---
+++ AvatarView/AvatarView.h
@@ -0,0 +1,19 @@
+//
+//  AvatarView.h
+//  AvatarView
+//
+//  Created by ChristianEnevoldsen on 08/06/15.
+//  Copyright (c) 2015 che. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AvatarView.
+FOUNDATION_EXPORT double AvatarViewVersionNumber;
+
+//! Project version string for AvatarView.
+FOUNDATION_EXPORT const unsigned char AvatarViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AvatarView/PublicHeader.h>
+
+
